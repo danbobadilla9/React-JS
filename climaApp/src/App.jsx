7a3d106b3fe917d28bfe33d522146cc2675e83a6
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-01-25 21:04:55 -0600

    25/01/23

---
+++ climaApp/src/App.jsx
@@ -0,0 +1,14 @@
+import AppClima from "./Components/AppClima"
+import { ClimaProvider} from './Context/ClimaProvider'
+function App() {
+    return (
+        <ClimaProvider>
+            <header>
+                <h1>Buscador de Clima</h1>
+            </header>
+            <AppClima/>            
+        </ClimaProvider>
+    )
+}
+
+export default App
