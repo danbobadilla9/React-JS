c20a25e8a44da47347ffd1794f196525e8dd3493
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-01-26 23:25:49 -0600

    nuevo-Proyecto

---
+++ notiaciasapi/src/context/NoticiasProvider.jsx
@@ -0,0 +1,33 @@
+import {useState, useEffect, createContext} from 'react';
+import axios from 'axios';
+const NoticiasContext = createContext();
+
+const NoticiasProvider = ({children}) => {
+
+    const[categoria,setCategoria] = useState('general');
+    const [noticias,setNoticias] = useState([])
+    useEffect( () => {
+        const consultarAPI = async() => {
+            const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apikey=${import.meta.env.VITE_API_KEY}`;
+            const { data } = await axios(url);
+            setNoticias(data.articles);
+        }
+        consultarAPI();
+    },[categoria]);
+
+    const handleChangeCategoria = e => {
+        setCategoria(e.target.value);
+    }
+
+    return(
+        <NoticiasContext.Provider value={{categoria,handleChangeCategoria,noticias}}>
+            {children}
+        </NoticiasContext.Provider>
+    )
+}
+
+export {
+    NoticiasProvider
+}
+
+export default NoticiasContext;
