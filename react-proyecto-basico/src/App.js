7246357961b527c84ac00aa696687f630ea0b090
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-02-01 23:51:28 -0600

    Agregando un manejador de archivos

--- react-proyecto-basico/src/App.js
+++ react-proyecto-basico/src/App.js
@@ -1,33 +1,24 @@
-
-const Video = props => {
-    return React.createElement("div",{},[
-        React.createElement("h2",{},props.title),
-        React.createElement("h3",{},props.dateAdded),
-        React.createElement("h3",{},props.channel)
-    ]);
-}
+import Video from './Video'
 
 const App = () => {
-    return React.createElement("div",{},[
-        React.createElement("h2",{},"Buscandor Video"),
-        React.createElement(Video,{
-            title:"Muse Sing for Absolution",
-            dateAdded:"23/01/23",
-            channel:"Muse"
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, "Buscandor Video"),
+        React.createElement(Video, {
+            title: "Muse Sing for Absolution",
+            dateAdded: "23/01/23",
+            channel: "Muse",
         }),
-        React.createElement(Video,{
-            title:"Muse Uno",
-            dateAdded:"23/11/21",
-            channel:"Muse"
+        React.createElement(Video, {
+            title: "Muse Uno",
+            dateAdded: "23/11/21",
+            channel: "Muse",
         }),
-        React.createElement(Video,{
-            title:"Muse Death Inside",
-            dateAdded:"22/81/99",
-            channel:"Muse"
-        })
+        React.createElement(Video, {
+            title: "Muse Death Inside",
+            dateAdded: "22/81/99",
+            channel: "Muse",
+        }),
     ]);
-}
+};
 
-
-
-ReactDOM.render(React.createElement(App),document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
---
+++ react-proyecto-basico/src/Video.js
@@ -0,0 +1,9 @@
+const Video = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, props.title),
+        React.createElement("h3", {}, props.dateAdded),
+        React.createElement("h3", {}, props.channel),
+    ]);
+};
+
+export default Video;
