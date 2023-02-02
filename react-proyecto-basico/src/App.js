e3594725e6e74a04bfed89d5375ff98017d283d7
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-02-01 22:31:05 -0600

    Proyecto con react antig

---
+++ react-proyecto-basico/src/App.js
@@ -0,0 +1,14 @@
+
+const Video = () => {
+    return (
+
+    );
+}
+
+const App = () => {
+    return React.createElement("div",{},React.createElement("h2",{},"Buscandor Video"));
+}
+
+
+
+ReactDOM.render(React.createElement(App),document.getElementById("root"));
