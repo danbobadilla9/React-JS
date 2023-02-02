232ce50c559455ecb54035cf233395d116db3919
Dan Bobadilla <danbobadilla9@hotmail.com>
2023-02-01 23:18:15 -0600

    Esta es la forma de trabajar sin el jsx

--- react-proyecto-basico/src/App.js
+++ react-proyecto-basico/src/App.js
@@ -1,12 +1,31 @@
 
-const Video = () => {
-    return (
-
-    );
+const Video = props => {
+    return React.createElement("div",{},[
+        React.createElement("h2",{},props.title),
+        React.createElement("h3",{},props.dateAdded),
+        React.createElement("h3",{},props.channel)
+    ]);
 }
 
 const App = () => {
-    return React.createElement("div",{},React.createElement("h2",{},"Buscandor Video"));
+    return React.createElement("div",{},[
+        React.createElement("h2",{},"Buscandor Video"),
+        React.createElement(Video,{
+            title:"Muse Sing for Absolution",
+            dateAdded:"23/01/23",
+            channel:"Muse"
+        }),
+        React.createElement(Video,{
+            title:"Muse Uno",
+            dateAdded:"23/11/21",
+            channel:"Muse"
+        }),
+        React.createElement(Video,{
+            title:"Muse Death Inside",
+            dateAdded:"22/81/99",
+            channel:"Muse"
+        })
+    ]);
 }
 
 
